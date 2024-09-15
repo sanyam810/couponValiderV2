a91c88d9f371c22af7b1d4a7434d58202b4189f4
sanyam <sanyamsaini081@gmail.com>
2024-09-15 18:11:41 +0530

    Refactor classnames function in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+//yo mr.white, this is the classnames function
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
