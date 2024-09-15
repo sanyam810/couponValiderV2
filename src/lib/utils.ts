95a41bb6ded255e71df256678d6ec2d2bfce988c
sanyam <sanyamsaini081@gmail.com>
2024-09-15 18:47:02 +0530

    Refactor cn function to optimize classnames generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Yo mr.white, I'm gonna need you to cook me up some classnames
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
