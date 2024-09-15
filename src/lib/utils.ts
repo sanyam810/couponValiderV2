fe502746dc419561c960f50c860007f1e8b0dff2
sanyam <sanyamsaini081@gmail.com>
2024-09-15 18:17:15 +0530

    Refactor utils.ts to optimize class merging

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+//yo mr.white, hows it going?
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
